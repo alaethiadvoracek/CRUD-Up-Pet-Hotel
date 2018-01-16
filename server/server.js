c12c3234b15eafe43f76934323233fadb9e114f6
Alaethia <truthcharis@gmail.com>
2018-01-16 14:39:30 -0600

    setup complete

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require( 'express' );
+const app = express();
+const bodyParser = require( 'body-parser' );
+const hotelRouter = require('./routes/hotel.router');
+
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('server/public'));
+
+
+// Routes
+
+
+const port = process.env.PORT || 5000;
+//Server Listener
+app.listen(port, function(){
+    console.log('server running on: ', port);
+  }); // end spin up server
